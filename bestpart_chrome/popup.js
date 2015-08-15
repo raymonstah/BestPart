cefcd2c770fe1112c079951ad01042f6d23cbac0
Raymond Ho <rayho@ucdavis.edu>
2015-08-15 11:44:13 -0700

    Able to get HTML video time back.
    Removed injection file, cleaned up JS.

--- bestpart_chrome/popup.js
+++ bestpart_chrome/popup.js
@@ -1,32 +1,30 @@
 $(document).ready(function() {
 	// This is for YouTube
-	$("#saveTime").click(function(){
-		// Returns the player time in seconds (not so accurate)
-		// var currentTime = $('#movie_player').getCurrentTime();
-		// This is how you set the time
-		//$('#movie_player').seekTo(currentTime, true);
-
+	$("#saveTime").click(function() {
 		// Asynchronous
-		chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
+		chrome.tabs.query({
+			active: true,
+			lastFocusedWindow: true
+		}, function(tabs) {
 			// Since there can only be one active tab in one active window, 
 			var tabURL = tabs[0].url;
-			$('#url').text(tabURL);
-			var message = document.querySelector('#message');
-			console.log(tabs[0]);
-			chrome.tabs.executeScript(null, {
-				code: "getPagesSource.js"
-			}, function() {
-				// If you try and inject into an extensions page or the webstore/NTP you'll get an error
-				if (chrome.runtime.lastError) {
-					message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
-				}
-			});
+			$('#url').text('Current URL: ' + tabURL);
+			getCurrentTime();
 		});
-	}); 
-});
+	});
 
-chrome.runtime.onMessage.addListener(function(request, sender) {
-  if (request.action == "getSource") {
-    message.innerText = request.source;
-  }
+
+	// A function that injects JS into the active tab to get video's current time back
+	var getCurrentTime = function() {
+		chrome.tabs.executeScript(null, {
+			code: 'var v = document.getElementsByTagName("video")[0]; v.currentTime'
+				// The last call in 'code' gets returned into 'results'
+		}, function(results) {
+			// Check errors
+			if (chrome.runtime.lastError) {
+				console.log(chrome.runtime.lastError.message);
+			}
+			$('#currentTime').text("Current time is: " + results);
+		});
+	}
 });
