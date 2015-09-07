b06ba3b786b2ac5fa2c594b573d6006eaeccf9df
Raymond Ho <rayho@ucdavis.edu>
2015-09-07 12:52:53 -0700

    Modularized? Javascript
    Organized everything.

--- bestpart_chrome/popup.js
+++ bestpart_chrome/popup.js
@@ -2,9 +2,30 @@
 // 8/22/15
 
 
+// The main module in charge of creating and adding tags.
 var TagModule = {
 
 	votingHTML: '<span><a href="#" class="img-upvote"><img src="thumbs-up.png" alt=""></a><span class="vote-count">9999</span><a href="#" class="img-downvote"><img src="thumbs-down.png"></a></span>',
+
+	createNewTag: function(url) {
+		var tm = this;
+
+		chrome.tabs.executeScript(null, {
+			code: 'var v = document.getElementsByTagName("video")[0]; v.currentTime'
+				// The last call in 'code' gets returned into 'results'
+		}, function(results) {
+			var time = results[0];
+			var description = $('#tagDesc').val();
+			if (description !== "") {
+				$('#tagDesc').val("");
+				tm.addTagToDom(time, description, 0);
+				ServerModule.sendTagToServer(url, time, description);
+
+			} else {
+				$('#tagDesc').attr("placeholder", "Please enter a tag..");
+			}
+		});
+	},
 
 	// Manipulates the DOM by adding a tag as a list item.
 	addTagToDom: function(time, description, score) {
@@ -40,23 +61,14 @@ var TagModule = {
 	}
 }
 
-$(function() {
+// Controls interaction with server-side.
+var ServerModule = {
+	HOSTURL: 'http://localhost:3000/bestpart/',
 
-	var HOSTURL = 'http://localhost:3000/bestpart/';
-
-	var tabURL = '';
-	chrome.tabs.query({
-		active: true,
-		lastFocusedWindow: true
-	}, function(tabs) {
-		// Since there can only be one active tab in one active window, 
-		tabURL = tabs[0].url;
-		$('#url').text('Current URL: ' + tabURL);
-		// Load the video's tags..
-		$.get(HOSTURL + "gettags", {
-			url: tabURL
+	getTagsFromServer: function(videoURL) {
+		$.get(this.HOSTURL + "gettags", {
+			url: videoURL
 		}, function(data) {
-			console.log(data);
 			if (data.error) {
 				$('#message').text("No tags found! Create some!");
 			} else {
@@ -64,65 +76,41 @@ $(function() {
 				tags.forEach(function(tag, index) {
 					console.log(tag);
 					var time = tag.tag_time;
-					var score = tag.tag_score;
 					var description = tag.tag_description;
+					var score = tag.tag_score;
 					TagModule.addTagToDom(time, description, score);
 				});
 			}
 		});
-	});
+	},
 
+	sendTagToServer: function(url, time, description) {
+		// Send this to the server.
+		tagJSON = {
+			url: url,
+			time: time,
+			description: description
+		};
+		$.post(this.HOSTURL + "tag", tagJSON);
+	},
+}
+
+$(function() {
+
+	var tabURL = '';
+
+	// Get the active Chrome tab and its URL.
+	chrome.tabs.query({
+		active: true,
+		lastFocusedWindow: true
+	}, function(tabs) {
+		// Since there can only be one active tab in one active window, 
+		tabURL = tabs[0].url;
+		$('#url').text('Current URL: ' + tabURL);
+		ServerModule.getTagsFromServer(tabURL);
+	});
 
 	$("#saveTime").click(function() {
-		addTagToDom(tabURL);
+		TagModule.createNewTag(tabURL);
 	});
-
-	// A function that injects JS into the active tab to get video's current time back
-	var addTagToDom = function(url) {
-		chrome.tabs.executeScript(null, {
-			code: 'var v = document.getElementsByTagName("video")[0]; v.currentTime'
-				// The last call in 'code' gets returned into 'results'
-		}, function(results) {
-			var time = results[0];
-			var timeString = secondsToHms(time);
-			var description = $('#tagDesc').val();
-			if (description !== "") {
-				$('#tagDesc').val("");
-				// Binds a event click to the list item.
-				var tagLink = $("<li>")
-					.text(timeString + " : " + description).attr("id", time);
-				tagLink.click(function() {
-					var time = $(this).attr('id');
-					changeToTime(time);
-				});
-				$('#tags').append(tagLink);
-
-				// Send this to the server.
-				tagJSON = {
-					url: url,
-					time: time,
-					description: description
-				};
-				$.post(HOSTURL + "tag", tagJSON);
-
-			} else {
-				$('#tagDesc').attr("placeholder", "Please enter a tag..");
-			}
-		});
-	};
-
-	// Changes the HTML5 video's time to the one passed in.
-	var changeToTime = function(time) {
-		chrome.tabs.executeScript(null, {
-			code: 'var v = document.getElementsByTagName("video")[0]; v.currentTime=' + time
-		}, function(results) {});
-	};
-
-	var secondsToHms = function(d) {
-		d = Number(d);
-		var h = Math.floor(d / 3600);
-		var m = Math.floor(d % 3600 / 60);
-		var s = Math.floor(d % 3600 % 60);
-		return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s);
-	}
 });
